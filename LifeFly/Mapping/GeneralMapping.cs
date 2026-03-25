3f78cd7db88235ab6fc5a1e50d645cdf7d5c514e
hayatullah alkis <hayati3449@gmail.com>
2026-03-26 01:17:26 +0300

    Uçuş DTO’ları, entity, eşleme, MongoDB ve admin arayüzü eklendi
    CRUD işlemleri için DTO’larla birlikte MongoDB destekli Flight entity’si oluşturuldu.
    Entity ile DTO eşlemesi için AutoMapper profili eklendi.
    Proje referansları AutoMapper, FluentValidation ve MongoDB desteğiyle güncellendi.
    AdminLayout, filtreleme, sayfalama, modal ve toast bildirimleri içeren istemci taraflı bir uçuş yönetim tablosuyla geliştirildi

---
+++ LifeFly/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LifeFly.Dtos.FlightDtos;
+using LifeFly.Entities;
+
+namespace LifeFly.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+       public GeneralMapping()
+        {
+            CreateMap<Flight, CreateFlightDto>().ReverseMap();
+            CreateMap<Flight, UpdateFlightDto>().ReverseMap();
+            CreateMap<Flight, GetFlightByIdDto>().ReverseMap();
+            CreateMap<Flight, ResultFlightDto>().ReverseMap();
+        }
+    }
+}
