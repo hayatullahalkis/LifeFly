9e92bec9631bb4f3547714b5bb93d59e0461d4bf
hayatullah alkis <hayati3449@gmail.com>
2026-03-26 00:27:00 +0300

    Yönetim paneli için etkileşimli havayolu dashboard arayüzüne sahip bir Admin alanı eklendi.
    Bu güncelleme ile, modern bir havayolu yönetim paneli oluşturmak amacıyla area yapısına uygun yönlendirme, controller’lar ve modüler view bileşenleri projeye dahil edildi.

---
+++ LifeFly/Areas/Admin/Controllers/FlightsListController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifeFly.Areas.Admin.Controllers
+{
+    public class FlightsListController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult FlightList()
+        {
+            return View();
+        }
+
+        public IActionResult CreateFligth()
+        {
+            return View();
+        }
+    }
+}
